cf0b6d077dbc3daac7087d6263d42c9fc5d3d5f0
Kin Pi <kinlampi@gmail.com>
2018-06-04 17:18:33 -0400

    Added circular profile pictures to bet item in bet list fragment.

--- app/src/main/java/com/kin/betmanager/adapters/BetsAdapter.java
+++ app/src/main/java/com/kin/betmanager/adapters/BetsAdapter.java
@@ -8,8 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.kin.betmanager.R;
 import com.kin.betmanager.activities.BetDetailActivity;
 import com.kin.betmanager.database.DatabaseHelper;
@@ -17,6 +19,8 @@ import com.kin.betmanager.objects.Bet;
 import com.kin.betmanager.objects.Contact;
 
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 /**
  * Created by Kin on 3/16/18.
@@ -59,6 +63,20 @@ public class BetsAdapter extends RecyclerView.Adapter<BetsAdapter.ViewHolder> {
             contact = holder.contact;
         }
         bettingAgainstTextView.setText(contact.name);
+
+        ImageView imageView = cardView.findViewById(R.id.profile_picture);
+        if (contact == null || contact.image.isEmpty()) {
+            Glide.with(context)
+                    .load(R.drawable.default_user)
+                    .bitmapTransform(new CropCircleTransformation(context))
+                    .into(imageView);
+        }
+        else {
+            Glide.with(context)
+                    .load(contact.image)
+                    .bitmapTransform(new CropCircleTransformation(context))
+                    .into(imageView);
+        }
 
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
